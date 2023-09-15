7176a11820ea964ae7cf99d48194b1a5f1882b06
ravicintl <140408587+ravicintl@users.noreply.github.com>
2023-09-15 21:22:55 +0530

    Updated header with Navbar

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -4,46 +4,44 @@ import styles from "./main-header.module.css";
 
 const MainHeader = () => {
   return (
-    <div className={styles.frameParent}>
-      <div className={styles.frameGroup}>
-        <div className={styles.homeWrapper}>
-          <button className={styles.home}>Home</button>
-        </div>
-        <button className={styles.learningsWrapper}>
-          <div className={styles.learnings}>Learnings</div>
-        </button>
-        <button className={styles.projectsWrapper}>
-          <div className={styles.learnings}>Projects</div>
-        </button>
-        <button className={styles.servicesWrapper}>
-          <div className={styles.learnings}>Services</div>
-        </button>
-        <button className={styles.pricingWrapper}>
-          <i className={styles.pricing}>Pricing</i>
-        </button>
-      </div>
-      <div className={styles.groupParent}>
+    <header className={styles.header}>
+      <div className={styles.logo}>
         <img className={styles.groupIcon} alt="" src="/group@2x.png" />
         <div className={styles.codersParent}>
           <b className={styles.coders}>Coder’s</b>
           <i className={styles.oneStop}>One Stop</i>
         </div>
       </div>
-      <div className={styles.rightHeader}>
-        <div className={styles.rectangleParent}>
-          <div className={styles.groupChild} />
-          <div className={styles.rectangleGroup}>
-            <div className={styles.groupItem} />
-            <i className={styles.moon}>moon</i>
-            <i className={styles.sunBright}>sun-bright</i>
-          </div>
+      <nav className={styles.navbar}>
+        <div className={styles.homeWrapper}>
+          <div className={styles.coders}>Home</div>
         </div>
-        <button className={styles.register}>Register</button>
-        <div className={styles.signInWrapper}>
-          <button className={styles.home}>Sign In</button>
+        <div className={styles.homeWrapper}>
+          <div className={styles.coders}>Learnings</div>
         </div>
+        <div className={styles.homeWrapper}>
+          <div className={styles.coders}>Projects</div>
+        </div>
+        <div className={styles.homeWrapper}>
+          <div className={styles.coders}>Services</div>
+        </div>
+        <div className={styles.homeWrapper}>
+          <i className={styles.pricing}>Pricing</i>
+        </div>
+      </nav>
+      <div className={styles.signinregister}>
+      <input className={styles.dark} checked={true} type="radio" />
+      <button className={styles.register}>
+          <div className={styles.signIn}>Register</div>
+        </button>
+        
+      <button >
+        <div className={styles.signin}>
+          <div className={styles.signIn}>Sign In</div>
+        </div>
+      </button>
       </div>
-    </div>
+    </header>
   );
 };
 
